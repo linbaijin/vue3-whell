9fea486c7e302e41880bbf583a33a0aac137c8f8
LeeBriken <576117134@qq.com>
2021-05-23 16:04:40 +0800

    项目初始化

---
+++ src/views/Doc.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <Topnav />
+        <div class="content">
+            <aside>
+                <ol>
+                    <li>组件1</li>
+                    <li>组件2</li>
+                </ol>
+            </aside>
+            <main>内容</main>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Topnav from '../components/Topnav.vue'
+export default {
+    name: 'Doc',
+    components: { Topnav },
+}
+</script>
+
+<style lang="scss">
+aside {
+    background: lightblue;
+    width: 150px;
+    padding: 16px;
+    position: fixed;
+    top: 0;
+    left: 0;
+    & > h2 {
+        margin-bottom: 4px;
+    }
+    & > ol {
+        & > li {
+            padding: 4px 0;
+        }
+    }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <Topnav />
+        <div class="banner">
+            <h1>L's UI</h1>
+            <h1>Asome Vue3 UI Framework</h1>
+            <p class="actions">
+                <a href="">Github</a>
+                <a href="/doc">开始</a>
+            </p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Topnav from '../components/Topnav.vue'
+export default {
+    name: 'Home',
+    components: { Topnav },
+}
+</script>
+
+<style lang="scss">
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions{
+    padding: 8px 0;
+    a {
+      margin:0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
+</style>
