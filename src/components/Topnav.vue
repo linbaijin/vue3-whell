9f64018431fbd23c9a2da41a580c5dc5a2101f0f
LeeBriken <576117134@qq.com>
2021-06-21 15:51:35 +0800

    手机切换按钮

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="topnav">
-    <div class="logo" @click="toggleMenuVisible">LOGO</div>
+    <div class="logo" >LOGO</div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
     </ul>
+    <span class="toggleAside" @click="toggleMenuVisible"></span>
   </div>
 </template>
 
@@ -31,6 +32,8 @@ export default {
   padding: 16px;
   position: relative;
   z-index: 10;
+  justify-content: center;
+  align-items: center;
   > .logo {
     max-width: 6em;
     margin-right: auto;
@@ -41,6 +44,27 @@ export default {
     flex-wrap: nowrap;
     > li {
       margin: 0 1em;
+    }
+  }
+  > .toggleAside {
+    display: none;
+    width: 24px;
+    height: 24px;
+    background-color: red;
+    position: absolute;
+    left: 16px;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+  @media (max-width:500px) {
+     > .menu {
+      display: none;
+    }
+    > .logo {
+      margin: 0 auto;
+    }
+    > .toggleAside {
+      display: inline-block;
     }
   }
 }
