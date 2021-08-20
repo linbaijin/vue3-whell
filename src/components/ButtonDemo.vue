3031fed6c9829661d45bb46d49bb9e47874be9d1
LeeBriken <576117134@qq.com>
2021-08-20 11:14:26 +0800

    完善button

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -6,6 +6,33 @@
     <Button theme="link">按钮</Button>
     <Button theme="text">按钮</Button>
   </div>
+  <h1>示例二</h1>
+  <div>
+    <Button size="small">按钮</Button>
+    <Button >按钮</Button>
+    <Button size="large">按钮</Button>
+  </div>
+  <h1>示例三</h1>
+  <div>
+    <Button type="primary">按钮</Button>
+    <Button type="success">按钮</Button>
+    <Button type="warning">按钮</Button>
+    <Button type="info">按钮</Button>
+    <Button type="danger">按钮</Button>
+  </div>
+  <h1>示例四</h1>
+  <div>
+    <Button type="primary" :disabled="true">按钮</Button>
+    <Button type="success" :disabled="true">按钮</Button>
+    <Button type="warning" :disabled="true">按钮</Button>
+    <Button type="info" :disabled="true">按钮</Button>
+    <Button type="danger" :disabled="true">按钮</Button>
+  </div>
+  <h1>示例五</h1>
+  <div>
+    <Button loading>按钮</Button>
+    <Button>加载完毕</Button>
+  </div>
 </template>
 
 <script lang="ts">
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,23 +1,50 @@
 <template>
-  <button class="ls-button" :class="`ls-theme-${theme}`" v-bind="rest">
-      <slot/>
+  <button class="ls-button" :class="className" :disabled="disabled">
+    <span v-if="loading" class="ls-loadingIndicator"></span>
+    <slot />
   </button>
 </template>
 
 <script lang="ts">
 import { computed } from 'vue'
 export default {
-    props: {
-        theme: {
-            type: String,
-            default: 'button'
-        }
+  props: {
+    theme: {
+      type: String,
+      default: 'button',
     },
-    setup(props, content) {
-        const className = computed(() => {
-            return {}
-        })
+    size: {
+      type: String,
+      default: 'normal',
+    },
+    type: {
+      type: String,
+      default: 'default',
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup(props, content) {
+    const { theme, size, type, disabled, loading } = props
+    const className = computed(() => {
+      return {
+        [`ls-theme-${theme}`]: theme,
+        [`ls-size-${size}`]: size,
+        [`ls-type-${type}`]: type,
+      }
+    })
+    return {
+      className,
+      disabled,
+      loading,
     }
+  },
 }
 </script>
 
@@ -36,19 +63,18 @@ $radius: 4px;
   justify-content: center;
   align-items: center;
   white-space: nowrap;
-  background: transparent;
-  color: $color;
-  border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  outline: none;
+  border: none;
   & + & {
     margin-left: 8px;
   }
-  &:hover,
-  &:focus {
-    color: $blue;
-    border-color: $blue;
-  }
+  // &:hover,
+  // &:focus {
+  //   color: $blue;
+  //   border-color: $blue;
+  // }
   &:focus {
     outline: none;
   }
@@ -56,22 +82,134 @@ $radius: 4px;
     border: 0;
   }
   &.ls-theme-button {
-      background-color: #fff;
+    background-color: #fff;
+    // &.ls-size-large {
+    //   font-size: 24px;
+    //   height: 48px;
+    // }
+    // &.ls-size-small {
+    //   font-size: 12px;
+    //   height: 24px;
+    // }
   }
-  &.ls-theme-link{
+  &.ls-theme-link {
     border-color: transparent;
     box-shadow: none;
     color: $blue;
-    &:hover,&:focus{
+    &:hover,
+    &:focus {
       color: lighten($blue, 10%);
     }
   }
-  &.ls-theme-text{
+  &.ls-theme-text {
     border-color: transparent;
     box-shadow: none;
     color: inherit;
-    &:hover,&:focus{
-      background: darken(white, 5%);;
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);
+    }
+  }
+  &.ls-theme-button {
+    &.ls-size-normal {
+      font-size: 16px;
+      height: 32px;
+    }
+    &.ls-size-large {
+      font-size: 24px;
+      height: 48px;
+    }
+    &.ls-size-small {
+      font-size: 12px;
+      height: 24px;
+    }
+    &.ls-type-default {
+      background: #fff;
+      color: $color;
+      border: 1px solid $border-color;
+      &[disabled] {
+        cursor: not-allowed;
+        color: #c0c4cc;
+      }
+    }
+    &.ls-type-primary {
+      background-color: $primary-color;
+      color: #fff;
+      &:hover,
+      &:focus {
+        background-color: lighten($primary-color, 10%);
+      }
+      &[disabled] {
+        background-color: $primary-disabled-color;
+        cursor: not-allowed;
+      }
+    }
+    &.ls-type-success {
+      background-color: $success-color;
+      color: #fff;
+      &:hover,
+      &:focus {
+        background-color: lighten($success-color, 10%);
+      }
+      &[disabled] {
+        background-color: $success-disabled-color;
+        cursor: not-allowed;
+      }
+    }
+    &.ls-type-warning {
+      background-color: $warning-color;
+      color: #fff;
+      &:hover,
+      &:focus {
+        background-color: lighten($warning-color, 10%);
+      }
+      &[disabled] {
+        background-color: $warning-disabled-color;
+        cursor: not-allowed;
+      }
+    }
+    &.ls-type-info {
+      background-color: $info-color;
+      color: #fff;
+      &:hover,
+      &:focus {
+        background-color: lighten($info-color, 10%);
+      }
+      &[disabled] {
+        background-color: $info-disabled-color;
+        cursor: not-allowed;
+      }
+    }
+    &.ls-type-danger {
+      background-color: $danger-color;
+      color: #fff;
+      &:hover,
+      &:focus {
+        background-color: lighten($danger-color, 10%);
+      }
+      &[disabled] {
+        background-color: $danger-disabled-color;
+        cursor: not-allowed;
+      }
+    }
+  }
+  > .ls-loadingIndicator {
+    width: 14px;
+    height: 14px;
+    display: inline-block;
+    margin-right: 4px;
+    border-radius: 8px;
+    border-color: $blue $blue $blue transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: ls-spin 1s infinite linear;
+  }
+  @keyframes ls-spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
     }
   }
 }
