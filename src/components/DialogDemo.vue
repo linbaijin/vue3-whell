90bc6675c4d4e9cfa2b1ed47cb3c47230ce049ed
LeeBriken <576117134@qq.com>
2021-08-20 15:30:12 +0800

    dialog示例

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,17 +1,38 @@
 <template>
-  <div>
-    <Dialog />
-  </div>
+  <div>Dialog示例</div>
+  <h1>示例一</h1>
+  <Button @click="toggle" type="primary">toggle</Button>
+  <Dialog v-model:visible="x"></Dialog>
+  <Dialog v-model:visible="x" :closeOnClickOverlay="false" :ok="f1" :cancel="f2"></Dialog>
 </template>
 
 <script lang="ts">
-// import { ref } from 'vue'
+import { ref } from 'vue'
 import Dialog from '../lib/Dialog.vue'
+import Button from '../lib/Button.vue'
 export default {
   components: {
     Dialog,
+    Button,
+  },
+  setup() {
+    const x = ref(false)
+    const toggle = () => {
+      x.value = !x.value
+    }
+    const f1 = () => {
+      return false
+    }
+    const f2 = () => {}
+    return {
+      x,
+      toggle,
+      f1,
+      f2,
+    }
   },
 }
 </script>
 
-<style></style>
+<style lang="scss">
+</style>
