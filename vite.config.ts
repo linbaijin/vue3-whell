ffe66b7c27398344f9469b3814dd7500d58c76fd
LeeBriken <576117134@qq.com>
2021-08-20 11:13:12 +0800

    添加公共样式

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+    scss: {
+      additionalData: '@import "./src/assets/scss/base.scss";' // 添加公共样式
+    }
+  },
+  },
   plugins: [vue()]
 })
