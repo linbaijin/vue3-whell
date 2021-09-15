22c88881ecb88a4b9b2fcf2d3f42e04739b822cd
LeeBriken <576117134@qq.com>
2021-09-15 14:08:04 +0800

    高亮当前路由

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -3,6 +3,15 @@
     <Topnav class="nav" />
     <div class="content">
       <aside :class="{ active: menuVisible }">
+        <h2>文档</h2>
+        <ol>
+          <li>
+            <router-link to="/doc/intro">介绍</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/get-started">开始</router-link>
+          </li>
+        </ol>
         <h2>组件列表</h2>
         <ol>
           <li>
@@ -51,42 +60,78 @@ export default {
   & > .content {
     position: relative;
     margin-top: 60px;
-    padding-left: 156px;
+    // padding-left: 156px;
+    padding-left: 0;
     flex-grow: 1;
-    @media (max-width: 500px) {
-      padding-left: 0;
-    }
+    // @media (max-width: 500px) {
+    //   padding-left: 0;
+    // }
   }
 }
 
 .content {
   display: flex;
   position: relative;
-  > main {
+  color: $doc-font-color;
+  & > aside {
+    @media (max-width: 500px) {
+      position: absolute;
+    }
+  }
+  & > main {
     flex-grow: 1;
     padding: 16px;
-    background: lightgreen;
   }
 }
 
 aside {
-  position: absolute;
+  position: relative;
   z-index: 10;
   flex-shrink: 0;
-  background: lightblue;
   width: 150px;
-  padding: 16px;
+  padding: 16px 0;
   top: 0;
   left: -100%;
   padding-top: 70px;
   height: 100%;
   transition: all 0.3s ease-in-out;
+  border-right: 1px solid rgba(61, 61, 61, 0.2);
   & > h2 {
+    padding: 0 16px;
     margin-bottom: 4px;
   }
   & > ol {
     & > li {
-      padding: 4px 0;
+      & > a {
+        position: relative;
+        display: block;
+        padding: 12px 16px;
+        outline: none;
+        background-color: #fff;
+        transition: all 0.3s ease-in-out;
+        &.router-link-active {
+          color: rgb(158, 71, 250);
+          background-color: rgba(217, 181, 255, 0.616);
+          &::after {
+            content: '';
+            position: absolute;
+            top: 0;
+            right: 0;
+            width: 3px;
+            height: 100%;
+            background-color: rgb(135, 22, 255);
+            animation: asideCurrent 250ms ease-in-out;
+          }
+        }
+      }
+    }
+  }
+  @keyframes asideCurrent {
+    from {
+        transform: scaleY(0);
+    }
+    to {
+        transform: scaleY(1);
     }
   }
 }
