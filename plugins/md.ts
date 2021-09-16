96d413ccef906df525df601a983b9d3ef1624b4c
LeeBriken <576117134@qq.com>
2021-09-16 14:16:20 +0800

    vite2 md解析器插件

---
+++ plugins/md.ts
@@ -0,0 +1,20 @@
+import path from 'path'
+import marked from 'marked'
+
+const compileMdToJs = (str: string) => {
+  const content = JSON.stringify(marked(str))
+  return `export default ${content}`
+}
+
+//https://cn.vitejs.dev/guide/api-plugin.html#transforming-custom-file-types
+export function md() {
+  return {
+    name: 'md-compiler',
+    transform(src, id) {
+      return {
+        code: id.endsWith('.md') ? compileMdToJs(src) : src,
+        map: null, // 如果可行将提供 source map
+      }
+    },
+  }
+}
