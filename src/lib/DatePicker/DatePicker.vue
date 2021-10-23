378b9d83dab1990007be014e9cc93858d45c37f1
LeeBriken <576117134@qq.com>
2021-10-23 17:31:24 +0800

    datepicker popover面包显示隐藏

--- src/lib/DatePicker/DatePicker.vue
+++ src/lib/DatePicker/DatePicker.vue
@@ -1,10 +1,16 @@
 <template>
-    <div class="ls-date-picker">
-        datepicker
+    <div class="ls-date-picker" ref="datePicker">
+        <div class="ls-date-picker__input" @click="onClickInput">
+            <input type="text" readonly>
+        </div>
+        <div class="ls-date-picker__popover" v-show="popoverVisible">
+            popover
+        </div>
     </div>
 </template>
 
-<script>
+<script lang="ts">
+import { onBeforeUnmount, onMounted, ref } from 'vue';
 export default {
     name: 'DatePicker',
     props: {
@@ -14,13 +20,44 @@ export default {
 
     },
     setup(props, context) {
-        return {
+        const datePicker = ref<HTMLDivElement>(null)
+        const popoverVisible = ref(false)
+        const onClickBody:EventListener = (e:MouseEvent) => {
+            console.log(e.target)
+           if(datePicker.value.contains(e.target as Node)) {
+               return
+           }
+           popoverVisible.value = false
+        }
+        const onClickInput = () => {
+            popoverVisible.value = true
+        }
+        onMounted(() => {
+            console.log(datePicker.value)
+            document.body.addEventListener('click', onClickBody)
+        })
+        onBeforeUnmount(() => {
 
+        })
+        return {
+            datePicker,
+            popoverVisible,
+            onClickInput
         };
     },
 };
 </script>
 
 <style lang="scss">
-
+.ls-date-picker {
+    &__input {
+        padding: 10px 12px;
+        box-shadow: 0 1px 3px rgb(50 50 93 / 15%), 0 1px 0 rgb(0 0 0 / 2%);
+        & > input {
+            color: #8898aa;
+            border: none;
+            outline: none;
+        }
+    }
+}
 </style>
