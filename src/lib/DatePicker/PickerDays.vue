377a07290f95b270cc667e980d6fffc95ad0e4cd
LeeBriken <576117134@qq.com>
2021-10-25 14:12:40 +0800

    新建pickerDays组件

---
+++ src/lib/DatePicker/PickerDays.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="ls-picker-days">
+        pickerDays
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    props: {
+
+    },
+    components: {
+
+    },
+    setup(props, context) {
+        return {
+
+        };
+    },
+};
+</script>
+
+<style lang="scss">
+
+</style>
