a6b181ff4265152092d8239351994d1a40c6392b
LeeBriken <576117134@qq.com>
2021-10-25 15:30:06 +0800

    获取当前月最后一天，上一个月最后一天的日期

--- src/lib/DatePicker/tools.ts
+++ src/lib/DatePicker/tools.ts
@@ -10,3 +10,11 @@ export const cloneDate = (date: Date | string) => {
 export const getYearMonthDay = (date:Date) => {
     return [date.getFullYear(), date.getMonth(), date.getDate()]
 }
+
+export const getCurrentMonthLastDay = (year: number, month: number) => {
+  return new Date(year, month + 1, 0).getDate()
+}
+
+export const getPreMonthLastDay = (year: number, month: number) => {
+  return new Date(year, month, 0).getDate()
+}
